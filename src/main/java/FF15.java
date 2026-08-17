211964f52aae07cdfc04fa1954382b809138fd6d
My2h <thawmyathtoo@email.com>
2026-08-17 16:26:00 +0800

    Renamed Chatbot

---
+++ src/main/java/FF15.java
@@ -0,0 +1,10 @@
+public class FF15 {
+    public static void main(String[] args) {
+        String banner = " _____ _____ _  ____  \n"
+                + "|  ___|  ___/ |/ ___| \n"
+                + "| |_  | |_  | |\\___ \\ \n"
+                + "|  _| |  _| | | ___) |\n"
+                + "|_|   |_|   |_||____/ \n";
+        System.out.println(banner);
+    }
+}
