f2bb0ac300fb498e8ac981e92eb8f6e823969163
My2h <thawmyathtoo@email.com>
2026-08-17 20:17:40 +0800

    Add, List

--- src/main/java/FF15.java
+++ src/main/java/FF15.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FF15 {
@@ -9,22 +10,43 @@ public class FF15 {
                 + "|_|   |_|   |_||____/ \n";
         String line = "____________________________________________________________";
 
-        System.out.println(line);
+        printDivider(line);
         System.out.println(banner);
-        System.out.println("Eh hello ah, I'm FF15 lah!");
-        System.out.println("What can I do for you sia?");
-        System.out.println(line);
+        printMessage("Eh hello bro, I'm FF15 !");
+        printMessage("What can I do for you big man ?");
+        printDivider(line);
+        System.out.println();
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Scanner object to receive input
         String input = scanner.nextLine();
+        ArrayList<String> list = new ArrayList<>(); // Container To-Do List
 
         while (!input.equals("bye")) {
-            System.out.println(input);
-            System.out.println(line);
+            if (input.equals("list")) {
+                printDivider(line);
+                for (int i = 0; i < list.size(); i++) {
+                    printMessage((i + 1) + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                printDivider(line);
+                printMessage("added: " + input);
+            }
+            printDivider(line);
+            System.out.println();
             input = scanner.nextLine();
         }
 
-        System.out.println("Okok bye bye, see you again soon ah!");
-        System.out.println(line);
+        printDivider(line);
+        printMessage("Okok bye bye, see you again soon !");
+        printDivider(line);
+    }
+
+    private static void printDivider(String divider) {
+        System.out.println("    " + divider);
+    }
+
+    private static void printMessage(String message) {
+        System.out.println("     " + message);
     }
 }
