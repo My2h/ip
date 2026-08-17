36ab01c1cc112517fb8718f298ba400d73ccd39b
My2h <thawmyathtoo@email.com>
2026-08-17 19:49:39 +0800

    Echo

--- src/main/java/FF15.java
+++ src/main/java/FF15.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class FF15 {
     public static void main(String[] args) {
         String banner = " _____ _____ _  ____  \n"
@@ -9,10 +11,20 @@ public class FF15 {
 
         System.out.println(line);
         System.out.println(banner);
-        System.out.println("Hello! I'm FF15.");
-        System.out.println("What can I do for you?");
+        System.out.println("Eh hello ah, I'm FF15 lah!");
+        System.out.println("What can I do for you sia?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            System.out.println(line);
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Okok bye bye, see you again soon ah!");
         System.out.println(line);
     }
 }
