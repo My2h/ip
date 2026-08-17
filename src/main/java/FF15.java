f554ab683018eed058abbe354adbcd6a4336beef
My2h <thawmyathtoo@email.com>
2026-08-17 16:32:12 +0800

    Added Greet and exit

--- src/main/java/FF15.java
+++ src/main/java/FF15.java
@@ -5,6 +5,14 @@ public class FF15 {
                 + "| |_  | |_  | |\\___ \\ \n"
                 + "|  _| |  _| | | ___) |\n"
                 + "|_|   |_|   |_||____/ \n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm FF15.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
